9640502f29171270aaddb14b86d825b915d46308
theBeginner86 <pranavsingh02@hotmail.com>
2021-07-30 04:26:39 +0530

    improved and added more child components

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,32 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React, {useState} from 'react';
+import { BrowserRouter as Router, Switch, Route, useHistory, Redirect } from 'react-router-dom';
+
 import HomePage from './components/home-page';
-import MyNavbar from './components/navbar';
-import SignUp from './components/signup';
+import MyNavbar from './components/navbar/navbar';
+import SignUp from './components/signup/signup';
+import SignIn from './components/signin/signin';
+
 
 function App() {
+  const history = useHistory();
+
+  const [logoutStatus, setlogoutStatus] = useState(true);
+
+  const logOutHandle = async () => {
+    setlogoutStatus(false);
+    localStorage.removeItem("token");
+    // console.log(history);
+    // history.push("/");
+    // return <Redirect to="/"/>
+  }
+
   return (
     <Router>
-        <MyNavbar/>
+        <MyNavbar logOutHandle={logOutHandle}/>
         <Switch>
           <Route path='/' component={HomePage} exact/>
           <Route path='/signup' component={SignUp} />
+          <Route path="/signin" component={SignIn}/>
         </Switch>
     </Router>
   );
