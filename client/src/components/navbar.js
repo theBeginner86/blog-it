830075ffb2454a5c07f44a78d7d7f54a966921eb
theBeginner86 <pranavsingh02@hotmail.com>
2021-07-30 00:34:47 +0530

    navbar improved

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Navbar, Container } from 'react-bootstrap';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 
 function MyNavbar(props) { 
     return ( 
         <div>
             <Navbar bg='light'>
                 <Container>
-                    <Navbar.Brand href="/">
-                        Blog It
-                    </Navbar.Brand>
+                    <Navbar.Brand href="/">Blog It</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="/signup">Register</Nav.Link>
+                    </Nav>
                 </Container>
             </Navbar>
         </div>
