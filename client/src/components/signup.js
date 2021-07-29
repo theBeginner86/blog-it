974cb46c90e420088088d5b28396dcc147b87d69
theBeginner86 <pranavsingh02@hotmail.com>
2021-07-30 00:34:02 +0530

    signup frontend created

--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -1,7 +1,12 @@
-import React, { useState } from 'react';
-import {BrowserRouter as Router, Redirect, useHistory, Link} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import {Redirect, useHistory} from 'react-router-dom';
+import {signupNewUser} from '../api/userApi';
 
 function SignUp(props) {
+
+    // useEffect(() => {
+    //     setSignupResponse(props);
+    // }, [props])
 
     const history = useHistory();
 
@@ -10,7 +15,10 @@ function SignUp(props) {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    function handleSubmit(event){
+    const [signupResponse, setSignupResponse] = useState("");
+
+    async function handleSubmit(event){
+
         console.log("inside handel submit");
         event.preventDefault();
         const userData = {
@@ -20,34 +28,48 @@ function SignUp(props) {
             password
         };
 
-        console.log("Submitted successfully", " user: ", userData);
-        history.push("/");
-        // return <Redirect to="/"/>
+        try {
+            const { data } = await signupNewUser(userData);
+            setSignupResponse(data);
+            console.log(data);
+            console.log(signupResponse);
+            console.log(userData);
+
+            if(data.success){
+                console.log("Submitted successfully", " user: ", userData);
+                history.push("/");
+                return <Redirect to="/"/>
+            }
+            
+        } catch (err) {
+            setSignupResponse(err);
+        }
     };
 
     
     return ( 
-        <Router>
-
         <div className="container signup content">
+
             <div  className="signup-heading">
                 <h1>Signup</h1>
             </div>
             <div className="container">
                 <form onSubmit={handleSubmit} className="signup-form">
                     <div className="container input-values">
+                        {
+                            (!signupResponse.success) ?  <div className="err-msg">{signupResponse.message}</div> : <div className="err-msg"></div>
+                        }
+                       
                         <input type="text" name="firstName" placeholder="First Name" value={firstName} onChange={(e) => setFirstName(e.target.value)}/><br/>
                         <input type="text" name="lastName" placeholder="Last Name" value={lastName} onChange={(e) => setLastName(e.target.value)}/><br/> 
                         <input type="text" name="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/><br/> 
                         <input type="text" name="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/><br/>
-                        <Link to="/"><button type="submit" className="submit-btn">Submit</button></Link>
+                        <button type="submit" className="submit-btn">Submit</button>
                     </div>       
                 </form>
             </div>
     
         </div>
-
-        </Router>
     );
     
 }
