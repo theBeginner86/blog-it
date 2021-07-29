accd394f53394174e537da8b2198ce525cb28983
theBeginner86 <pranavsingh02@hotmail.com>
2021-07-29 12:52:45 +0530

    added token functionality

---
+++ server/utils/authenticateToken.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
+
+function authenticateToken(req, res, next){
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if(token == null) return res.send({
+        success: false,
+        message: "Error: Empty Token"
+    });
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if(err) return res.send({
+            success: false,
+            message: "Error: Server Error"
+        })
+
+        req.user = user;
+        next();
+    })
+}
+
+function generateAccessToken(user){
+    return jwt.sign({user}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '15s'});
+}
+
+module.exports = {authenticateToken, generateAccessToken};
