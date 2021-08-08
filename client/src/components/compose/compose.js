04983157c44ca74d90e7c0f4eff46e3a8004ba3d
theBeginner86 <pranavsingh02@hotmail.com>
2021-08-08 19:12:01 +0530

    improved UI of compose page

--- client/src/components/compose/compose.js
+++ client/src/components/compose/compose.js
@@ -41,17 +41,18 @@ function Compose(props){
 
     return (
         <div className="content" id="#compose-blog">
+        <h1 className="heading-per-page">Compose</h1>
             <div className="blog-content">
                 <div className="main main-raised">
                     <div className="container blog">
-                        <h1 className="title">Compose</h1>
-                        <hr/>
+                        {/* <h1 className="title">Compose</h1> */}
                         <form onSubmit={handleSubmit} className="compose-area">
                         {
                             (!composeResponse.success) ?  <div className="err-msg">{composeResponse.message}</div> : <div className="success-msg">Blog Saved Successfully</div>
                         }
                             <input className="title" type="text" name="title" value={title} placeholder="Title" onChange={(e) => setTitle(e.target.value)}/><br/>
-                            <textarea type="text" cols="111" rows="10" name="blog" value={blog} placeholder="Use this area to compose your blog" onChange={(e) => setBlog(e.target.value)}/><br/>
+                            <hr/>
+                            <textarea className="text-area" type="text" cols="111" rows="10" name="blog" value={blog} placeholder="Use this area to compose your blog" onChange={(e) => setBlog(e.target.value)}/><br/>
                             <hr Style={"margin-top:1rem"}/>
                             <div className="submit-btn">
                                 <button type="submit" onClick={handleSubmit}>Submit</button>
